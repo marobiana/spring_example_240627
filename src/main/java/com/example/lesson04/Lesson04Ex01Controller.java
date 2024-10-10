8e7ae1b3e22d1269e7a804fd136726316079f982
test <test@test.com>
2024-10-10 19:52:03 +0900

    모델 들어가기 전 일부 코드

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,6 +37,17 @@ public class Lesson04Ex01Controller {
 		
 		// 결과 화면 이동
 		return "lesson04/afterAddUser";
+	}
+	
+	// 최신 가입자 정보 화면
+	// http://localhost/lesson04/ex01/latest-user-view
+	@GetMapping("/latest-user-view")
+	public String latestUserView() {
+		// DB select
+		
+		// Model 데이터를 집어넣는다.
+		
+		return "lesson04/latestUser"; // view 경로
 	}
 }
 
